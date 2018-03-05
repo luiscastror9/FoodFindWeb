b06d9e75c5e0685532b86d9e29633b31389ddbf5
lzgbrl <37087614+lzgbrl@users.noreply.github.com>
2018-03-05 19:58:02 -0300

    Agregar archivos de proyecto.

---
+++ yoCocinoWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace yoCocinoWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ yoCocinoWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="yoCocinoWeb.MvcApplication" Language="C#" %>
